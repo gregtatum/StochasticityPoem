5157454c8a8f2b344c3d4ec0daf7256d20156efd
Greg Tatum <tatum.creative@gmail.com>
2014-03-17 16:13:10 -0500

    Vector Flow is working, tweaks to follow

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 var TwoScene = function() {
 	
 	this.div = document.getElementById( 'container' );
-	this.$canvas = $('canvas');
-	this.canvas = this.$canvas.get(0);
+	this.canvasColors = document.getElementById("colors");
+	this.canvasVectors = document.getElementById("vectors");
+	
+	this.$canvasColors = $( this.canvasColors );
+	this.$canvasVectors = $( this.canvasVectors );
+	
 	this.ratio = window.devicePixelRatio >= 1 ? window.devicePixelRatio : 1;
-	this.context = this.canvas.getContext( '2d' );
+	this.contextColors = this.canvasColors.getContext( '2d' );
+	this.contextVectors = this.canvasVectors.getContext( '2d' );
 	
 	this.multiplyChance = 1 / 240;
-	
-	this.vectorFlow = new VectorFlow( this );
-
-	//this.addStats();
-	this.addEventListeners();
+	this.stats = true;
+	this.maxWalkers = 500;
 	
 	this.resizeCanvas();
+	this.addEventListeners();
+
+	PERLIN.noise.seed( Math.random() );
 	
+	this.addStats();
+	this.vectorFlow = new VectorFlow( this, this.contextVectors );
 	this.addWalkers( 1 );
 	
-	this.maxWalkers = 500;
 	
 	this.loop();
 };
@@ -29,7 +35,7 @@ TwoScene.prototype = {
 		this.walkers = [];
 		
 		for(var i=0; i < number; i++) {
-			this.walkers[i] = new Walker( this );
+			this.walkers[i] = new Walker( this, this.contextColors, this.vectorFlow );
 		}
 	},
 	
@@ -45,7 +51,7 @@ TwoScene.prototype = {
 		if( Math.random() <= this.multiplyChance && this.walkers.length < this.maxWalkers ) {
 			
 			oldWalker = this.walkers[ Math.floor( this.walkers.length * Math.random() ) ];
-			newWalker = new Walker( this );
+			newWalker = new Walker( this, this.contextColors, this.vectorFlow );
 			
 			this.walkers.push( newWalker );
 			
@@ -56,6 +62,7 @@ TwoScene.prototype = {
 	},
 	
 	addStats : function() {
+		if(!this.stats) return;
 		this.stats = new Stats();
 		this.stats.domElement.style.position = 'absolute';
 		this.stats.domElement.style.top = '0px';
@@ -67,14 +74,18 @@ TwoScene.prototype = {
 	},
 	
 	resizeCanvas : function(e) {
-		this.canvas.width = $(window).width();
-		this.canvas.height = $(window).height();
-		this.width = this.canvas.width;
-		this.height = this.canvas.height;
-		this.left = this.$canvas.offset().left;
-		this.top = this.$canvas.offset().top;
 		
-		console.log(this.width, this.height);
+		this.width = $(window).width();
+		this.height = $(window).height();
+		
+		this.canvasColors.width = this.width;
+		this.canvasColors.height = this.height;
+		
+		this.canvasVectors.width = this.width;
+		this.canvasVectors.height = this.height;
+		
+		this.left = this.$canvasColors.offset().left;
+		this.top = this.$canvasColors.offset().top;
 	},
 			
 	loop : function() {
@@ -101,22 +112,26 @@ TwoScene.prototype = {
 	},
 	
 	render : function() {
-		//this.stats.update();
+		if(this.stats) this.stats.update();
 		
 		//this.context.clearRect(0,0,this.width, this.height);
 		
 		this.drawWalkers();
 		
-		this.context.fillStyle = this.rgbToFillStyle(245, 245, 245, 0.05);
-		this.context.fillRect(0,0,this.width, this.height);
-		this.context.fill();
+		//this.contextColors.fillStyle = this.rgbToFillStyle(245, 245, 245, 0.001);
+		//this.contextColors.fillRect(0,0,this.width, this.height);
 		
+		//Vectors
+		this.contextVectors.clearRect(0,0,this.width, this.height);
 		this.vectorFlow.drawCells();
 	}
 	
 };
 
-var Walker = function(scene) {
+var Walker = function( scene, context, vectorFlow ) {
+	this.context = context;
+	this.vectorFlow = vectorFlow;
+	
 	this.ratio = 0.5;
 	this.scene = scene;
 	this.x = 0;
@@ -125,8 +140,6 @@ var Walker = function(scene) {
 	this.hueStep = 3;
 	this.size = 5;
 	this.hue = (Math.random() * 45) + this.hueStart;
-	
-	
 };
 
 Walker.prototype = {
@@ -138,8 +151,13 @@ Walker.prototype = {
 	},
 	
 	update : function() {
-		this.x += Math.random() * this.moveStep / this.ratio;
-		this.y += this.random() * this.moveStep * this.ratio;
+		
+		var length = Math.random() * this.moveStep;
+		
+		var cell = this.vectorFlow.getCellAtSceneCoordinate(this.x, this.y);
+		
+		this.x += Math.cos( cell.theta ) * this.moveStep;
+		this.y += Math.sin( cell.theta ) * this.moveStep;
 		this.hue += this.random() * this.hueStep;
 		
 		this.x %= this.scene.width;
@@ -156,28 +174,33 @@ Walker.prototype = {
 	},
 	
 	draw : function() {
-		this.scene.context.beginPath();
-		this.scene.context.fillStyle = this.scene.hslToFillStyle(this.hue, 100, 50, 0.5);
-		this.scene.context.fillRect(
+		this.context.beginPath();
+		this.context.fillStyle = this.scene.hslToFillStyle(this.hue, 100, 50, 0.5);
+		this.context.fillRect(
 			this.x,
 			this.y,
 			this.size / this.ratio,
 			this.size * this.ratio
 		);
-		this.scene.context.fill();
+		this.context.fill();
 	}
 	
 };
 
-var VectorFlow = function(scene) {
+var VectorFlow = function( scene, context ) {
 	this.scene = scene;
-	this.targetCellWidth = 200;
+	this.context = context;
+	this.targetCellWidth = 25;
 	
 	this.arrowLength = 0.5;
 	
-	this.strokeStyle = this.scene.rgbToFillStyle(0,0,0,0.2);
-	this.lineWidth = 2;
-	this.lineCap = "round";
+	this.strokeStyle = this.scene.rgbToFillStyle(0,0,0, 0.5);
+	this.lineWidth = 1;
+	this.lineCap = "butt";
+	
+	this.noiseDelta = 0;
+	this.noiseSpeed;
+	this.noiseScale;
 	
 	this.grid = {
 		cells : [],
@@ -185,7 +208,7 @@ var VectorFlow = function(scene) {
 		height : Math.ceil( $(window).height() / this.targetCellWidth ),
 		position : new THREE.Vector2(),
 		cellSize : new THREE.Vector2()
-	}
+	};
 	
 	this.generateGrid();
 	this.updateSceneCoordinates();
@@ -207,17 +230,12 @@ VectorFlow.prototype = {
 			for(j=0; j < this.grid.height; j++) {
 				
 				cell = {
-					
 					vector : this.setDirection( new THREE.Vector2(), position.set(i,j) ),
 					gridCoordinates : new THREE.Vector2(i, j),
 					sceneCoordinates : new THREE.Vector2(),
 					pixelCenter : new THREE.Vector2(),
-					theta : undefined
+					theta : PERLIN.noise.simplex3(i,j,0)
 				};
-				
-				cell.theta = Math.atan2( cell.vector.y, cell.vector.x );
-				cell.theta = Math.PI * 2 * Math.random();
-				cell.theta = 0;
 				
 				cells[ i ][ j ] = cell;
 			}
@@ -228,14 +246,12 @@ VectorFlow.prototype = {
 		
 		var cells = this.grid.cells,
 			i, j,
-			cell;
+			cell, averageCellSize;
 		
 		this.grid.cellSize = new THREE.Vector2(
 			$(window).width() / this.grid.width,
 			$(window).height() / this.grid.height
 		);
-		
-		console.log(this.grid.width, this.grid.height, this.grid.cellSize.x, this.grid.cellSize.y);
 		
 		for( i = 0; i < this.grid.width; i++) {
 			for(j=0; j < this.grid.height; j++) {
@@ -251,10 +267,12 @@ VectorFlow.prototype = {
 					.copy( this.grid.cellSize )
 					.multiplyScalar( 0.5 )
 					.add( cell.sceneCoordinates );
-					
-					console.log(i,j,cell.sceneCoordinates.x, cell.sceneCoordinates.y)
 			}
 		}
+
+		averageCellSize = ( this.grid.cellSize.x + this.grid.cellSize.y ) / 2;
+		this.noiseSpeed = 0.0001 / averageCellSize;
+		this.noiseScale = averageCellSize;
 	},
 	
 	setDirection : function( vector, position ) {
@@ -293,6 +311,7 @@ VectorFlow.prototype = {
 		this.drawTipR = this.drawPoints[3];
 			
 		this.arrowLengths = [];
+		this.arrowAngles = [];
 		
 		cellSize = this.grid.cellSize;
 		halfLengthY = (this.arrowLength * cellSize.y) / 2;
@@ -317,34 +336,40 @@ VectorFlow.prototype = {
 			(this.arrowLength * cellSize.y) / 4
 		);
 		
+		this.theta = 0;
+		
 		for(i=0; i < this.arrowPoints.length; i++) {
 			this.arrowLengths.push( this.arrowPoints[i].length() );
+			this.arrowAngles.push( Math.atan2( this.arrowPoints[i].x, this.arrowPoints[i].y ) );
 		}
 	},
 	
 	drawCell : function( cell ) {
-			
+		
+		this.noiseDelta += this.noiseSpeed;
+		
+		cell.theta = 2 * Math.PI * PERLIN.noise.simplex3(
+			cell.gridCoordinates.x / this.noiseScale,
+			cell.gridCoordinates.y / this.noiseScale,
+			this.noiseDelta
+		);
+		
 		//Rotate the points
 		for(i=0; i < this.arrowPoints.length; i++) {
 			
-			this.drawPoints[i].x = Math.cos( cell.theta ) * this.arrowLengths[i];
-			this.drawPoints[i].y = Math.sin( cell.theta ) * this.arrowLengths[i];
-			
-			debugger;
+			this.drawPoints[i].x = Math.cos( cell.theta + this.arrowAngles[i] ) * this.arrowLengths[i];
+			this.drawPoints[i].y = Math.sin( cell.theta + this.arrowAngles[i] ) * this.arrowLengths[i];
 			
 			this.drawPoints[i].add( cell.pixelCenter );
+			
 		}
 		
-		
 		//Draw the points
-		this.scene.context.moveTo(this.drawBase.x,	this.drawBase.y);
-		this.scene.context.lineTo(this.drawTip.x,	this.drawTip.x);
-		
-		
-		//this.scene.context.lineTo(this.drawTipL.x,	this.drawTipL.y);
-		//this.scene.context.moveTo(this.drawTip.x,	this.drawTip.y);
-		//this.scene.context.lineTo(this.drawTipR.x,	this.drawTipR.y);
-		this.scene.context.stroke();
+		this.context.moveTo(this.drawBase.x,	this.drawBase.y);
+		this.context.lineTo(this.drawTip.x,	this.drawTip.y);
+		this.context.moveTo(this.drawTipL.x,	this.drawTipL.y);
+		this.context.lineTo(this.drawTip.x,	this.drawTip.y);
+		this.context.lineTo(this.drawTipR.x,	this.drawTipR.y);
 		
 	},
 	
@@ -353,18 +378,43 @@ VectorFlow.prototype = {
 			position = new THREE.Vector2(),
 			cell;
 		
-		this.scene.context.strokeStyle = this.strokeStyle;
-		this.scene.context.lineWidth = this.lineWidth;
-		this.scene.context.lineCap = this.lineCap;
+		this.context.strokeStyle = this.strokeStyle;
+		this.context.lineWidth = this.lineWidth;
+		this.context.lineCap = this.lineCap;
 	
+		this.context.beginPath()
 		for( position.x = 0; position.x < this.grid.width; position.x++) {
 			for(position.y=0; position.y < this.grid.height; position.y++) {
 				
 				this.drawCell( cells[ position.x ][ position.y ] );
 			}
 		}
+		this.context.stroke();
+		this.context.closePath();
+	},
+	
+	coordinateSceneToGrid : function( sceneCoordinate ) {
 		
-		debugger;
+		var i, j, cellSize = this.grid.cellSize;
+				
+ 		i = Math.floor( sceneCoordinate.x / cellSize.x );
+		j = Math.floor( sceneCoordinate.y / cellSize.y );
+	},
+	
+	getCellAtSceneCoordinate : function( x, y ) {
+		
+		var i, j, cellSize = this.grid.cellSize;
+				
+ 		i = Math.floor( x / cellSize.x );
+		j = Math.floor( y / cellSize.y );
+		
+		i = Math.max(i, 0);
+		j = Math.max(j, 0);
+		
+		i = Math.min(i, this.grid.width - 1);
+		j = Math.min(j, this.grid.height - 1);
+		
+		return this.grid.cells[i][j];
 	}
 };
 
